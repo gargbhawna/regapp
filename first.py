df5b722e59b5bee59cc7b32afcfadd1e52741073
gargbhawna <46916770+gargbhawna@users.noreply.github.com>
2020-03-22 17:16:00 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -9,56 +9,48 @@ from flask import render_template
 
 app = Flask(__name__)
 app.config["MONGO_URI"] ="mongodb+srv://bhawna28:gargpass@cluster0-jte0f.mongodb.net/test?retryWrites=true&w=majority"
-app.config['RECAPTCHA_PUBLIC_KEY'] = ''
-app.config['RECAPTCHA_PRIVATE_KEY'] = ''
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcBD-MUAAAAACjhf8kmXMEpF0bdFwbz51aRFOi_'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcBD-MUAAAAABA6qizuw12lhxM14YqfZXcDitPr'
 mongo = PyMongo(app)
 
 
 @app.route('/', methods=['POST','GET'])
 def hello_world():
-    sitekey = ""
-    if request.method == 'POST':
-        client = pymongo.MongoClient("mongodb+srv://bhawna28:gargpass@cluster0-jte0f.mongodb.net/test?retryWrites=true&w=majority")
-        db = client.test_regis
-        col= db.user_details        
-        existing_user = col.find_one({'name' : request.form['name']}) or col.find_one({'email' : request.form['email']})
-        #print(users)
-        if existing_user:
-            return "Record already exists !!!"
-        try:
-            pre_records = col.find({"client_ip": request.remote_addr, "created": datetime.datetime.utcnow().strftime("%Y%m%d")})
-            if len(list(pre_records)) >= 3:
-                print("yes ..............")
 
-                render_template('user_captcha.html')
-                user_data  = {"name": request.form["name"], "email": request.form["email"], "password": request.form["psw"], "created": datetime.datetime.utcnow().strftime("%Y%m%d"), "client_ip": request.remote_addr}
-                captcha_response = request.form['g-recaptcha-response']    
-                print(captcha_response)         
-       
-                if is_human(captcha_response):
-                # Process request here
-                    col.insert_one(user_data)
-                    return "Detail submitted successfully."
-                else:
-                # Log invalid attempts
-                    return "Sorry ! Bots are not allowed."
-            user_data1  = {"name": request.form["name"], "email": request.form["email"], "password": request.form["psw"], "created": datetime.datetime.utcnow().strftime("%Y%m%d"), "client_ip": request.remote_addr}
-            col.insert_one(user_data1)
-            records = col.find()
-            to_send_rec = ''
-            for rec in records:
-                to_send_rec += str(rec)
-            return "Record added and records are : {}".format(str(to_send_rec))
-        except Exception as e:
-            print(str(e))
-            return 'Exception Occurred'
-
+    client = pymongo.MongoClient("mongodb+srv://bhawna28:gargpass@cluster0-jte0f.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.test_regis
+    col= db.user_details        
+    pre_records = col.find({"client_ip": request.remote_addr, "created": datetime.datetime.utcnow().strftime("%Y%m%d")})
+   
+    if len(list(pre_records)) >= 3:
+       if request.method == 'POST':
+           user_data  = {"name": request.form["name1"], "email": request.form["email1"], "password": request.form["psw1"], "created": datetime.datetime.utcnow().strftime("%Y%m%d"), "client_ip": request.remote_addr}
+           captcha_response = request.form.get('g-recaptcha-response')
+           existing_users = col.find_one({'name' : request.form['name1']}) or col.find_one({'email' : request.form['email1']})
+           if existing_users:
+               return "Record already exists !!!"
+           print(captcha_response)
+           if is_human(captcha_response):
+               col.insert(user_data)
+               return "Successfully Registered!!!"
+           else :
+               return "You are a bot!!"
+       return render_template('user_captcha.html')
+    else:
+        if request.method == 'POST':
+            user_data  = {"name": request.form["name"], "email": request.form["email"], "password": request.form["psw"], "created": datetime.datetime.utcnow().strftime("%Y%m%d"), "client_ip": request.remote_addr}
+            existing_users = col.find_one({'name' : request.form['name']}) or col.find_one({'email' : request.form['email']})
+            if existing_users:
+               return "Record already exists !!!"
+            col.insert(user_data)
+            return "Successfully Registered!!!"
+       #col.insert(user_data)
     return render_template('user.html')
 
 
 def is_human(captcha_response):
 
-    secret = ""
+    secret = "6LcBD-MUAAAAABA6qizuw12lhxM14YqfZXcDitPr"
     payload = {'response':captcha_response, 'secret':secret}
     response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
     response_text = json.loads(response.text)
